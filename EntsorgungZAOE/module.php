88c9915aa014bc3b7d4ab8d1447a8d2fe554a55a
poperzelchen <poperzelchen@gmx.de>
2017-01-22 13:57:43 +0100

    up

--- EntsorgungZAOE/module.php
+++ EntsorgungZAOE/module.php
@@ -12,7 +12,7 @@
 			$this->RegisterPropertyString("area", "789");
 			$this->RegisterPropertyString("ort", "916");
 			$this->RegisterPropertyString("strasse", "4411");
-			$this->RegisterTimer("Updatetonne", 15 * 60 * 1000, 'Tonne_Update(\$_IPS[\'TARGET\']);'); 
+			$this->RegisterTimer("Updatetonne", 15 * 60 * 1000, 'ZAOE_Update(\$_IPS[\'TARGET\']);'); 
 		}		
 	
 		public function ApplyChanges()
