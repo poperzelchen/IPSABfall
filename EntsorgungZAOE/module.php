f0f51182667bea066319b8fcb7a485af93eeedb0
poperzelchen <poperzelchen@gmx.de>
2017-01-22 17:40:57 +0100

    up

--- EntsorgungZAOE/module.php
+++ EntsorgungZAOE/module.php
@@ -24,7 +24,6 @@
 			$this->RegisterVariableString("BioTime", "Bioabfall 60-240l Tonne");
 			$this->RegisterVariableString("RecycleTime", "Gelbe Säcke/Gelbe Tonne");
 			$this->RegisterVariableString("PaperTime", "Papier/Pappe 120/240l Tonne");
-
 		}
 	
 		/**
@@ -37,15 +36,41 @@
 		
 		    public function Update()
     {
-            $jahr = date("Y") - 1;        
-            $this->SetValueString("WasteTime", $jahr);
-			$this->SetValueString("BioTime", "test1");
-			$this->SetValueString("RecycleTime", "test2");
-			$this->SetValueString("PaperTime", "test3");
+                
+        $jahr = date("Y");
+        $link = $this->'http://www.zaoe.de/ical/download/' . ReadPropertyString("strasse") . '/16/?tx_kalenderausgaben_pi3%5Bauswahl_start_us%5D='. $jahr . '-01-01&tx_kalenderausgaben_pi3%5Bauswahl_end_us%5D='. $jahr . '-12-31&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=2&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B4%5D=5&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B5%5D=6&tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=16';
+        $this->SendDebug('GET', $link, 0);
+        $meldung = @file($link);
+        if ($meldung === false)
+            throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
+        $this->SendDebug('LINES', count($meldung), 0);
+
+		$tonne = "Keine Tonne";
+		
+		$anzahl = (count($meldung) - 1);
+
+        for ($count = 0; $count < $anzahl; $count++)
+        {
+            if (strstr($meldung[$count], "SUMMARY:"))
+            {
+                $name = trim(substr($meldung[$count], 8));
+                $start = trim(substr($meldung[$count + 1], 19));
+                $ende = trim(substr($meldung[$count + 2], 17));
+                $this->SendDebug('SUMMARY', $name, 0);
+                $this->SendDebug('START', $start, 0);
+                $this->SendDebug('END', $ende, 0);
+                $jetzt = date("Ymd") . "\n";
+                if (($jetzt >= $start) and ( $jetzt <= $ende))
+                {
+                    $tonne = explode(' ', $name)[0];
+                    $this->SendDebug('FOUND', $tonne, 0);
+                }
+            }
+        }
+
+		return $tonne;
     }
 	
-	
-
 	private function SetValueString(string $Ident, string $value)
     {
         $id = $this->GetIDForIdent($Ident);
