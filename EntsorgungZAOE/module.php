42efc686d415b43685a153dbc8188e68cb5fd717
poperzelchen <poperzelchen@gmx.de>
2018-01-03 22:19:42 +0100

    2fd

--- EntsorgungZAOE/module.php
+++ EntsorgungZAOE/module.php
@@ -3,6 +3,10 @@
 	class Abfallkalender extends IPSModule
 	{
 
+	
+	
+	
+	
 		public function Create()
 		{
 			//Never delete this line!
@@ -27,7 +31,7 @@
 			$this->RegisterVariableString("RecycleTime", "Gelbe Säcke/Gelbe Tonne");
 			$this->RegisterVariableString("PaperTime", "Papier/Pappe 120/240l Tonne");
 		
-			$this->Update();
+		//	$this->Update();
 		}
 		/**
 		* This function will be available automatically after the module is imported with the module control.
@@ -36,16 +40,17 @@
 		* EZVO_RequestInfo($id);
 		*
 		*/
-		
+	 
 		   public function Update()
-    {
-        try
+		{
+	
+		     try
         {
             $TonneB = $this->GetTonneB()[0];
 			$AbholungB = $this->GetTonneB()[1];
 			$TonneP = $this->GetTonneP()[0];
 			$AbholungP = $this->GetTonneP()[1];
-			$TonneG = $this->GetTonneG()[0];
+		    $TonneG = $this->GetTonneG()[0];
 			$AbholungG = $this->GetTonneG()[1];
 			$TonneR = $this->GetTonneR()[0];
 			$AbholungR = $this->GetTonneR()[1];
@@ -57,85 +62,67 @@
             return false;
         }
 
-$TonneB = str_replace("Keine Tonne", "", $TonneB);
-$TonneP = str_replace("Keine Tonne", "", $TonneP);
-$TonneG = str_replace("Keine Tonne", "", $TonneG);
-$TonneR = str_replace("Keine Tonne", "", $TonneR);
+			$TonneB = str_replace("Keine Tonne", "", $TonneB);
+			$TonneP = str_replace("Keine Tonne", "", $TonneP);
+			$TonneG = str_replace("Keine Tonne", "", $TonneG);
+			$TonneR = str_replace("Keine Tonne", "", $TonneR);
 
-$laengeB = strlen($TonneB);
-$laengeP = strlen($TonneP);
-$laengeG = strlen($TonneG);
-$laengeR = strlen($TonneR);
+			$laengeB = strlen($TonneB);
+			$laengeP = strlen($TonneP);
+			$laengeG = strlen($TonneG);
+			$laengeR = strlen($TonneR);
 
 
-$TonneArray = array();
-if ($laengeB != 0) {
-$TonneArray[] =$TonneB;
-}
-if ($laengeP != 0) {
-$TonneArray[] =$TonneP;
-}
-if ($laengeG != 0) {
-$TonneArray[] =$TonneG;
-}
-if ($laengeR != 0) {
-$TonneArray[] =$TonneR;
-}
-$this->SetValue("BioTime", "arsch");
-$Tonne = implode(", ", $TonneArray);
+			$TonneArray = array();
+				if ($laengeB != 0) {
+				$TonneArray[] =$TonneB;
+				}
+				if ($laengeP != 0) {
+				$TonneArray[] =$TonneP;
+				}
+				if ($laengeG != 0) {
+				$TonneArray[] =$TonneG;
+				}
+				if ($laengeR != 0) {
+				$TonneArray[] =$TonneR;
+				}
+
+			$Tonne = implode(", ", $TonneArray);
 
 		
-		$this->SetValue("BioTime", $AbholungB);
-		$this->SetValue("PaperTime", $AbholungP);
-		$this->SetValue("RecycleTime", $AbholungG);
-		$this->SetValueS("WasteTime", $AbholungR);
+		$this->SetValueString("BioTime", $AbholungB);
+		$this->SetValueString("PaperTime", $AbholungP);
+		$this->SetValueString("RecycleTime", $AbholungG);
+		$this->SetValueString("WasteTime", $AbholungR);
 		if ($Tonne == "")
         {
-            $this->SetValue("IsAbholung", false);
-			$this->SetValue("tonne", "Keine Abholung");
+            $this->SetValueBoolean("IsAbholung", false);
+			$this->SetValueString("tonne", "Keine Abholung");
         }
         else
         {
-            $this->SetValue("IsAbholung", true);
-			$this->SetValue("tonne", $Tonne);
+            $this->SetValueBoolean("IsAbholung", true);
+			$this->SetValueString("tonne", $Tonne);
         }
         return true;
     }
-		
-		
-		
-		
+	
 			   public function GetTonneB()
-    
-{
- 
-		if ((int)date("md") < 110)
-        {
-            $jahr = date("Y") - 1;
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
-		
-			$this->SendDebug('GET', $link, 0);
-            $meldung = @file($link);
-            if ($meldung === false)
-                throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-            $this->SendDebug('LINES', count($meldung), 0);
-        } else
-        {
-            $meldung = array();
-        }       
-        $jahr = date("Y");
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
-		       
- 				
+    {
+		 
+	   
+        $jahr = substr(date("Y"),2);
+		//$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
+		$link = "https://www.zaoe.de/ical/" . $this->ReadPropertyString("strasse") . "/_3/" . $jahr ."/";
 		$this->SendDebug('GET', $link, 0);
-        $meldung2 = @file($link);
-        if ($meldung2 === false)
-            throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-        $this->SendDebug('LINES', count($meldung2), 0);
+        $meldung = @file($link);
+        if ($meldung === false)
+          throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
+        $this->SendDebug('LINES', count($meldung), 0);
 		
-		$meldung = array_merge($meldung, $meldung2);
 		$tonne = "Keine Tonne";
 		$tonnedate = "-";
+
 		
 		$anzahl = (count($meldung) - 1);
 
@@ -158,9 +145,9 @@ $Tonne = implode(", ", $TonneArray);
 				$jetzt6 = date("Ymd",time() + 518400);				
 				$jetzt7 = date("Ymd",time() + 604800);
 				if (($jetzt7 == $start) || ($jetzt6 == $start) || ($jetzt5 == $start) || ($jetzt4 == $start) || ($jetzt3 == $start) || ($jetzt2 == $start) || ($jetzt1 == $start) || ($jetzt == $start) )
-                { 
+                {
 					$tonnedate = date("d.m.Y", strtotime($start));
-				}
+                }
 				if (($jetzt +1 == $start))
                 {
 					$tonne = "Morgen " . explode(' ', $name) [0];
@@ -175,37 +162,24 @@ $Tonne = implode(", ", $TonneArray);
 			
         }
 	
-				return array($tonne,$tonnedate);
 		
-    }
+		return array($tonne,$tonnedate);
+		
+    } 
 	
 			   public function GetTonneP()
     {
- 
-		if ((int)date("md") < 110)
-        {
-            $jahr = date("Y") - 1;
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
+		 
+	    
+        $jahr = substr(date("Y"),2);
+		//$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
+		$link = "https://www.zaoe.de/ical/" . $this->ReadPropertyString("strasse") . "/_4/" . $jahr ."/";
+		$this->SendDebug('GET', $link, 0);
+        $meldung = @file($link);
+        if ($meldung === false)
+          throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
+        $this->SendDebug('LINES', count($meldung), 0);
 		
-			$this->SendDebug('GET', $link, 0);
-            $meldung = @file($link);
-            if ($meldung === false)
-                throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-            $this->SendDebug('LINES', count($meldung), 0);
-        } else
-        {
-            $meldung = array();
-        }       
-          $jahr = date("Y");
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
-		
-	$this->SendDebug('GET', $link, 0);
-        $meldung2 = @file($link);
-        if ($meldung2 === false)
-            throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-        $this->SendDebug('LINES', count($meldung2), 0);
-		
-		$meldung = array_merge($meldung, $meldung2);
 		$tonne = "Keine Tonne";
 		$tonnedate = "-";
 
@@ -253,108 +227,20 @@ $Tonne = implode(", ", $TonneArray);
 		
     }
 	
-			   public function GetTonneR()
-    {
- 
-		if ((int)date("md") < 110)
-        {
-		$jahr = date("Y") - 1;
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
-				
-			$this->SendDebug('GET', $link, 0);
-            $meldung = @file($link);
-            if ($meldung === false)
-                throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-            $this->SendDebug('LINES', count($meldung), 0);
-        } else
-        {
-            $meldung = array();
-        }       
-		$jahr = date("Y");
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
-		
-		
-		$this->SendDebug('GET', $link, 0);
-        $meldung2 = @file($link);
-        if ($meldung2 === false)
-            throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-        $this->SendDebug('LINES', count($meldung2), 0);
-		
-		$meldung = array_merge($meldung, $meldung2);
-		$tonne = "Keine Tonne";
-		$tonnedate = "-";
-
-		
-		$anzahl = (count($meldung) - 1);
-
-        for ($count = 0; $count < $anzahl; $count++)
-        {
-            
-			if (strstr($meldung[$count], "SUMMARY:Restabfall"))
-            {
-                $name = trim(substr($meldung[$count], 8));
-                $start = trim(substr($meldung[$count + 1], 19));
-                $ende = trim(substr($meldung[$count + 2], 17));
-                $this->SendDebug('SUMMARY', $name, 0);
-                $this->SendDebug('START', $start, 0);
-                $this->SendDebug('END', $ende, 0);
-                $jetzt = date("Ymd",time()) ;
-				$jetzt1 = date("Ymd",time() + 86400);
-				$jetzt2 = date("Ymd",time() + 172800);
-				$jetzt3 = date("Ymd",time() + 259200);
-				$jetzt4 = date("Ymd",time() + 345600);
-				$jetzt5 = date("Ymd",time() + 432000);
-				$jetzt6 = date("Ymd",time() + 518400);				
-				$jetzt7 = date("Ymd",time() + 604800);
-				if (($jetzt7 == $start) || ($jetzt6 == $start) || ($jetzt5 == $start) || ($jetzt4 == $start) || ($jetzt3 == $start) || ($jetzt2 == $start) || ($jetzt1 == $start) || ($jetzt == $start) )
-                 {
-					$tonnedate = date("d.m.Y", strtotime($start));
-                }
-				if (($jetzt +1 == $start))
-                {
-					$tonne = "Morgen " . explode(' ', $name) [0];
-					$this->SendDebug('FOUND', $tonne , 0);
-                }
-				elseif ($jetzt == $start)
-				{
-					$tonne = "Heute " . explode(' ', $name) [0];
-					$this->SendDebug('FOUND', $tonne , 0);
-                }
-            }
-        }
-			
-		return array($tonne,$tonnedate);
-	}
 			   public function GetTonneG()
     {
  
-		if ((int)date("md") < 110)
-        {
-       $jahr = date("Y") - 1;
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
-			$this->SendDebug('GET', $link, 0);
-            $meldung = @file($link);
-            if ($meldung === false)
-                throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-            $this->SendDebug('LINES', count($meldung), 0);
-        } else
-        {
-            $meldung = array();
-        }       
-      $jahr = date("Y");
-		$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
-		
-		
+		$jahr = substr(date("Y"),2);
+		//$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
+		$link = "https://www.zaoe.de/ical/" . $this->ReadPropertyString("strasse") . "/_6/" . $jahr ."/";
 		$this->SendDebug('GET', $link, 0);
-        $meldung2 = @file($link);
-        if ($meldung2 === false)
-            throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
-        $this->SendDebug('LINES', count($meldung2), 0);
+        $meldung = @file($link);
+        if ($meldung === false)
+          throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
+        $this->SendDebug('LINES', count($meldung), 0);
 		
-		$meldung = array_merge($meldung, $meldung2);
 		$tonne = "Keine Tonne";
 		$tonnedate = "-";
-
 		
 		$anzahl = (count($meldung) - 1);
 
@@ -400,6 +286,62 @@ $Tonne = implode(", ", $TonneArray);
     }	
 		
     
+	
+			   public function GetTonneR()
+    {
+		$jahr = substr(date("Y"),2);
+		//$link = 'https://www.zaoe.de/ical/download/' . $this->ReadPropertyString("strasse") . '/18/?tx_kalenderausgaben_pi3%5Bauswahl_start%5D=01.01.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_end%5D=31.12.'. $jahr . '&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B0%5D=1&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B1%5D=3&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B2%5D=4&tx_kalenderausgaben_pi3%5Bauswahl_tonnen_ids%5D%5B3%5D=6&tx_kalenderausgaben_pi3%5Bswitch%5D=ical&tx_kalenderausgaben_pi3%5Bauswahl_zeitraum%5D=18';
+		$link = "https://www.zaoe.de/ical/" . $this->ReadPropertyString("strasse") . "/_1/" . $jahr ."/";
+		$this->SendDebug('GET', $link, 0);
+        $meldung = @file($link);
+        if ($meldung === false)
+          throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
+        $this->SendDebug('LINES', count($meldung), 0);
+		
+		$tonne = "Keine Tonne";
+		$tonnedate = "-";
+		$anzahl = (count($meldung) - 1);
+
+        for ($count = 0; $count < $anzahl; $count++)
+        {
+            if (strstr($meldung[$count], "SUMMARY:Restabfall"))
+            {
+                $name = trim(substr($meldung[$count], 8));
+                $start = trim(substr($meldung[$count + 1], 19));
+                $ende = trim(substr($meldung[$count + 2], 17));
+                $this->SendDebug('SUMMARY', $name, 0);
+                $this->SendDebug('START', $start, 0);
+                $this->SendDebug('END', $ende, 0);
+                $jetzt = date("Ymd",time()) ;
+				$jetzt1 = date("Ymd",time() + 86400);
+				$jetzt2 = date("Ymd",time() + 172800);
+				$jetzt3 = date("Ymd",time() + 259200);
+				$jetzt4 = date("Ymd",time() + 345600);
+				$jetzt5 = date("Ymd",time() + 432000);
+				$jetzt6 = date("Ymd",time() + 518400);				
+				$jetzt7 = date("Ymd",time() + 604800);
+				if (($jetzt7 == $start) || ($jetzt6 == $start) || ($jetzt5 == $start) || ($jetzt4 == $start) || ($jetzt3 == $start) || ($jetzt2 == $start) || ($jetzt1 == $start) || ($jetzt == $start) )
+                 {
+					$tonnedate = date("d.m.Y", strtotime($start));
+                }
+				if (($jetzt +1 == $start))
+                {
+					$tonne = "Morgen  ". explode(' ', $name) [0];
+					$this->SendDebug('FOUND', $tonne , 0);
+                }
+				elseif ($jetzt == $start)
+				{
+					$tonne = "Heute " . explode(' ', $name) [0];
+					$this->SendDebug('FOUND', $tonne , 0);
+                }
+            }
+			
+        }
+	
+		
+		return array($tonne,$tonnedate);
+		
+    }
 	
 	  private function SetValueBoolean(string $Ident, bool $value)
     {
